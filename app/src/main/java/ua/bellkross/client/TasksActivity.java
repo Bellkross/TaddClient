e7e43ec4293a14e4c24d89676a4768151b00cccb
Bellkross <vasilenkowm@yandex.ua>
2017-12-15 13:16:10 +0200

    fix font size

--- app/src/main/java/ua/bellkross/client/TasksActivity.java
+++ app/src/main/java/ua/bellkross/client/TasksActivity.java
@@ -40,7 +40,6 @@ public class TasksActivity extends AppCompatActivity {
     private EditText etInputTask;
     private int roomID;
     private Boolean isFabOpen = false;
-    private FloatingActionButton fab,fab1,fab2;
     private Animation fab_open,fab_close,rotate_forward,rotate_backward;
 
     @Override
@@ -56,41 +55,11 @@ public class TasksActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
 
-
-        fab = (FloatingActionButton)findViewById(R.id.fab);
-        fab1 = (FloatingActionButton)findViewById(R.id.fab1);
-        fab2 = (FloatingActionButton)findViewById(R.id.fab2);
-
-
         fab_open = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
         fab_close = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fab_close);
 
         rotate_forward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_forward);
         rotate_backward = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_backward);
-
-        if (fab != null)
-            fab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    int id = v.getId();
-                    switch (id){
-                        case R.id.fab:
-
-                            animateFAB();
-                            break;
-                        case R.id.fab1:
-
-                            Log.d("Raj", "Fab 1");
-                            break;
-                        case R.id.fab2:
-
-                            Log.d("Raj", "Fab 2");
-                            break;
-                    }
-                }
-            });
-        //fab1.setOnClickListener((View.OnClickListener) this);
-        //fab2.setOnClickListener((View.OnClickListener) this);
 
         adapter = new RecyclerViewAdapter(this, ArrayListRooms.getInstance().get(roomID).getTasks(),
                 new View.OnClickListener() {
@@ -174,33 +143,6 @@ public class TasksActivity extends AppCompatActivity {
         adapter.refresh(roomID);
     }
 
-
-
-
-    public void animateFAB(){
-
-        if(isFabOpen){
-
-            fab.startAnimation(rotate_backward);
-            fab1.startAnimation(fab_close);
-            fab2.startAnimation(fab_close);
-            fab1.setClickable(false);
-            fab2.setClickable(false);
-            isFabOpen = false;
-            Log.d("Raj", "close");
-
-        } else {
-
-            fab.startAnimation(rotate_forward);
-            fab1.startAnimation(fab_open);
-            fab2.startAnimation(fab_open);
-            fab1.setClickable(true);
-            fab2.setClickable(true);
-            isFabOpen = true;
-            Log.d("Raj","open");
-
-        }
-    }
     public void deleteTask(int taskID) {
 
 
